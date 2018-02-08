17b87db503b5d0f4fb7c8470c5e2b56b300c53fc
Lyszard <lyszardkrol@gmail.com>
2018-02-08 20:46:30 +0100

    Final
    final

--- TASK_2/page_report.py
+++ TASK_2/page_report.py
@@ -3,22 +3,34 @@ import sys
 
 
 urlList = []
-
+emptyNumber = 0
+error = False
 with open(sys.argv[1], 'r') as f:
 
+    for  line in f:
+        if  line.strip() == "":
+            emptyNumber+=1
+            error = True
 
-    for line in f:
-        # ----finding url in a line----#
-        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
-        # ----removing the query string part----#
-        text = urls[0].split("?")
-        # ----removing ending slash----#
-        if text[0][-1] == "/":
-            text[0] = text[0][:-1]
-            urlList.append(text[0])
+
         else:
-            urlList.append(text[0])
- #----counting each url and adding it to a dictionary----#
+        # ----finding url in a line----#
+            urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+            # ----removing the query string part----#
+            text = urls[0].split("?")
+            # ----removing ending slash----#
+            if text[0][-1] == "/":
+                text[0] = text[0][:-1]
+                urlList.append(text[0])
+            else:
+                urlList.append(text[0])
+
+if error:
+    print >> sys.stderr, "Invalid log lines:%d " % emptyNumber
+else:
+    pass
+
+     #----counting each url and adding it to a dictionary----#
 occurences = dict((i, urlList.count(i)) for i in urlList)
 for item,value in occurences.items():
     print "\"%s\",%d \n"%(item,value)
